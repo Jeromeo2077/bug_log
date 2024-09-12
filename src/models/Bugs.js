8052c53643c617f04b331c9aef260f7abb7763ec
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-12 14:59:09 -0600

    Update to the BugsSchema to correct an incorrect type for the priority property

--- src/models/Bugs.js
+++ src/models/Bugs.js
@@ -3,7 +3,7 @@ import { Schema } from "mongoose";
 export const BugsSchema = new Schema({
   title: { type: String, minlength: 10, maxlength: 50, required: true },
   description: { type: String, minlength: 10, maxlength: 500, required: true },
-  priority: [{ type: Number, min: 1, max: 5, required: true }],
+  priority: { type: Number, min: 1, max: 5, required: true },
   closed: { type: Boolean, default: false, required: true },
   closedDate: { type: Date, required: false },
   creatorId: { type: Schema.ObjectId, ref: 'Account', required: true }
