086c55eee85ff321e564ce88b930bc211545de6c
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-12 14:18:53 -0600

    Created BugsSchema

--- src/controllers/BugsController.js
+++ src/controllers/BugsController.js
@@ -1,9 +1,10 @@
+import auth0provider from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
 
 export class BugsController extends BaseController {
   constructor() {
     super('api/bugs')
     this.router
-
+      .use(auth0provider.getAuthorizedUserinfo)
   }
 }
