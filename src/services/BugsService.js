12e9e54611d5d582790166423bdded2b3366d4fe
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-12 15:01:51 -0600

    added missing populate method in my createBug method

--- src/services/BugsService.js
+++ src/services/BugsService.js
@@ -3,7 +3,7 @@ import { dbContext } from "../db/DbContext.js"
 class BugsService {
 
   async createBug(bugData) {
-    const bug = await dbContext.Bugs.create(bugData)
+    const bug = (await dbContext.Bugs.create(bugData)).populate('creator')
     return bug
   }
 
